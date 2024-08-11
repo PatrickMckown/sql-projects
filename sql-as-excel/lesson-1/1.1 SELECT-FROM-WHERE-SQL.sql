2bcf2e05eb3f8146b372a24a09e544657b83e300
Patrick McKown <patrickmckown@outlook.com>
2024-08-11 11:44:09 -0700

    Update 1.1 SELECT-FROM-WHERE-SQL.sql
    Finished "WHERE" section, made some small formatting changes

--- sql-as-excel/lesson-1/1.1 SELECT-FROM-WHERE-SQL.sql
+++ sql-as-excel/lesson-1/1.1 SELECT-FROM-WHERE-SQL.sql
@@ -14,41 +14,41 @@ Imagine you have an Excel spreadsheet that has a table in it. This table contain
 +--------------+--------------+--------------+--------------+
 | SalesOrderID | OrderDate    | CustomerID   | TotalDue     |
 +--------------+--------------+--------------+--------------+
-| 1            | 2024-01-01   | 101          | 1000.00      |
-| 2            | 2024-01-02   | 102          | 1500.00      |
-| 3            | 2024-01-03   | 103          | 2000.00      |
-| 4            | 2024-01-04   | 104          | 2500.00      |
-| 5            | 2024-01-05   | 105          | 3000.00      |
+| 43659        | 5/31/2011    | 29825        | 23153.23     |
+| 43660        | 5/31/2011    | 29672        | 1457.33      |
+| 43661        | 5/31/2011    | 29734        | 36865.80     |
+| 43662        | 5/31/2011    | 29994        | 32474.93     |
+| 43663        | 5/31/2011    | 29565        | 472.31       |
 +--------------+--------------+--------------+--------------+
 
-Selecting a Column in Excel
-- In SQL: SELECT SalesOrderID
+Selecting a Column
 - In Excel: This is similar to highlighting the entire column labeled SalesOrderID. You click on the header of the SalesOrderID column, which highlights all the cells in that column. SELECT always comes before FROM.
+- In SQL: SELECT SalesOrderID
 
-Specifying the Table in Excel
-- In SQL: FROM Sales.SalesOrderHeader
+Specifying the Table
 - In Excel: This is equivalent to specifying that the data you are interested in is in a table named SalesOrderHeader within your Excel workbook. You navigate to the tab and range where the table is contained. FROM always comes after SELECT.
+- In SQL: FROM Sales.SalesOrderHeader
 
 So, if we write "SELECT SalesOrderID FROM Sales.SalesOrderHeader" we get the following:
 
 +--------------+
-| SalesOrderID | 
+| SalesOrderID |
 +--------------+
-| 1            | 
-| 2            | 
-| 3            |
-| 4            | 
-| 5            | 
+| 43659        |
+| 43660        |
+| 43661        |
+| 43662        |
+| 43663        |
 +--------------+
 
 Explain
 1. Explain what's so important about keywords
-2. Explain two keywords in SQL
+2. Explain two keywords in SQL, what do they do?
 3. Explain which keyword is like telling the database which columns you want
 */
 
-SELECT SalesOrderID
-FROM Sales.SalesOrderHeader
+SELECT SalesOrderID FROM Sales.SalesOrderHeader;
+GO
 
 
 /*
@@ -56,45 +56,79 @@ SELECT Multiple Columns
 
 To ask for more than one column, just separate the column names with a comma. 
 
-Imagine we have the same table as above.
+Excel Analogy
+Imagine we have the same table as above in our Excel workbook.
 
 +--------------+--------------+--------------+--------------+
 | SalesOrderID | OrderDate    | CustomerID   | TotalDue     |
 +--------------+--------------+--------------+--------------+
-| 1            | 2024-01-01   | 101          | 1000.00      |
-| 2            | 2024-01-02   | 102          | 1500.00      |
-| 3            | 2024-01-03   | 103          | 2000.00      |
-| 4            | 2024-01-04   | 104          | 2500.00      |
-| 5            | 2024-01-05   | 105          | 3000.00      |
+| 43659        | 5/31/2011    | 29825        | 23153.23     |
+| 43660        | 5/31/2011    | 29672        | 1457.33      |
+| 43661        | 5/31/2011    | 29734        | 36865.80     |
+| 43662        | 5/31/2011    | 29994        | 32474.93     |
+| 43663        | 5/31/2011    | 29565        | 472.31       |
 +--------------+--------------+--------------+--------------+
+
+Selecting Multiple Columns
+- In Excel: This is equivalent to holding down the "CTRL" button on the keyboard, left clicking on SalesOrderID column and then left clicking the OrderDate column
+- In SQL: To select more than we column, we write "SELECT SalesOrderID, OrderDate FROM Sales.SalesOrderHeader" 
 
 So, if we write "SELECT SalesOrderID, OrderDate FROM Sales.SalesOrderHeader" we get the following:
 
 +--------------+--------------+
 | SalesOrderID | OrderDate    |
 +--------------+--------------+
-| 1            | 2024-01-01   |
-| 2            | 2024-01-02   |
-| 3            | 2024-01-03   |
-| 4            | 2024-01-04   |
-| 5            | 2024-01-05   |
+| 43659        | 5/31/2011    |
+| 43660        | 5/31/2011    |
+| 43661        | 5/31/2011    |
+| 43662        | 5/31/2011    |
+| 43663        | 5/31/2011    |
 +--------------+--------------+
 
 Explain
-1. Explain which punctuation mark(s) to use to seperate column names
-2. Explain what "SELECT SalesOrderID, OrderDate FROM Sales.SalesOrderHeader" would look like if done in Excel
-
+1. Explain which punctuation mark(s) are used to seperate column names
+2. Explain what "SELECT SalesOrderID, TotalDue FROM Sales.SalesOrderHeader" would look like if done in Excel
+3. Explain how I would select the 3 left-most columns in both SQL and Excel
 */
 
-SELECT SalesOrderID, OrderDate
-FROM Sales.SalesOrderHeader
+SELECT SalesOrderID, OrderDate FROM Sales.SalesOrderHeader;
+GO
 
 /*
-
-
 WHERE
 
-WHERE is the third keyword that makes up most SQL statements.
+WHERE is the third keyword that makes up most SQL statements. WHERE goes after FROM.
+
+- WHERE: This keyword tells the database what specific values you want to see when you ask for data from the datasbase with SELECT and FROM.
+
+Excel Analogy
+Imagine you have the same Excel spreadsheet with the table below in it.
+
++----------------+---------------+--------------+--------------+
+| SalesOrderID ▼ | OrderDate ▼   | CustomerID ▼ | TotalDue ▼   |
++----------------+---------------+--------------+--------------+
+| 43659          | 5/31/2011     | 29825        | 23153.23     |
+| 43660          | 5/31/2011     | 29672        | 1457.33      |
+| 43661          | 5/31/2011     | 29734        | 36865.80     |
+| 43662          | 5/31/2011     | 29994        | 32474.93     |
+| 43663          | 5/31/2011     | 29565        | 472.31       |
++----------------+---------------+--------------+--------------+
+
+Now imagine you only want to see the orders that have a SalesOrderID of "43659".
+
+Filtering for a Value
+- In Excel: This is equivalent to clicking on the filter in the SalesOrderID column header and only filtering for "43659".
+- In SQL: To filter for a value we add the WHERE keyword, specify the column and then specify the value using a "=" sign
+
+So, if we write "SELECT SalesOrderID, TotalDue FROM Sales.SalesOrderHeader WHERE SalesOrderID = 43659" we get the following:
+
++----------------+--------------+
+| SalesOrderID ▼ | TotalDue ▼   |
++----------------+--------------+
+| 43659          | 23153.23     |
++----------------+--------------+
 
 */
 
+SELECT SalesOrderID, TotalDue FROM Sales.SalesOrderHeader WHERE SalesOrderID = 43659;
+GO
