264a9f98175452b05bef68952eb3347bf47d014d
Patrick McKown <patrickmckown@outlook.com>
2024-08-14 20:48:22 -0700

    Update Customer_Contact_Master.sql
    Restructure of query

--- queries/Customer_Contact_Master.sql
+++ queries/Customer_Contact_Master.sql
@@ -1,58 +1,33 @@
-WITH FactVendors AS (
-	SELECT
-		BusinessEntityID
-		, AccountNumber
-		, [Name]
-	FROM
-		Purchasing.Vendor
-)
-, FactCustomers AS (
-	SELECT
-		CustomerID
-		, StoreID
-		, TerritoryID
-	FROM
-		Sales.Customer
-)
-, DimAddressID AS (
-	SELECT
-		BusinessEntityID
-		, AddressID
-		, AddressTypeID
-	FROM
-		Person.BusinessEntityAddress
-)
-, DimAddressTypeID AS (
-	SELECT
-		AddressTypeID
-		, [Name] -- Address Description
-	FROM
-		Person.AddressType
-)
-, DimPersonContactID AS (
-	SELECT
-		BusinessEntityID
-		, PersonID
-		, ContactTypeID
-	FROM
-		Person.BusinessEntityContact
-)
-, CustomerContactMaster AS (
-	SELECT
-		fc.CustomerID
-		, fc.StoreID
-		, fc.TerritoryID
-		, daid.AddresstypeID
-		, datid.[Name] AS AddressDescription
-	FROM
-		FactCustomers AS fc
-	LEFT JOIN
-		DimAddressID AS daid ON fc.CustomerID = daid.BusinessEntityID
-	LEFT JOIN
-		DimAddressTypeID AS datid ON daid.AddressTypeID = datid.AddressTypeID
-		
-)
-
 SELECT
-	*
-FROM CustomerContactMaster
+    pa.AddressID
+	, pbea.BusinessEntityID
+    , pa.AddressLine1
+    , pa.City
+    , pa.PostalCode
+    , psp.[Name] AS ProvinceStateName
+	, pat.[Name] AS AddressType
+	, pea.EmailAddress
+	, ppp.PhoneNumber
+	, pp.FirstName
+	, pp.LastName
+	, sst.CountryRegionCode
+	, pcr.[Name] AS CountryName
+	, sst.[Group] AS CountryGroup
+FROM 
+    Person.[Address] AS pa
+INNER JOIN
+	Person.BusinessEntityAddress AS pbea ON pa.AddressID = pbea.AddressID
+INNER JOIN
+    Person.StateProvince AS psp ON pa.StateProvinceID = psp.StateProvinceID
+INNER JOIN
+	Person.AddressType AS pat ON pbea.AddressTypeID = pat.AddressTypeID 
+INNER JOIN
+	Person.EmailAddress AS pea ON pbea.BusinessEntityID = pea.BusinessEntityID
+INNER JOIN
+	Person.Person AS pp ON pbea.BusinessEntityID = pp.BusinessEntityID
+INNER JOIN
+	Person.PersonPhone AS ppp ON pbea.BusinessEntityID = ppp.BusinessEntityID
+INNER JOIN
+	Sales.SalesTerritory AS sst ON psp.TerritoryID = sst.TerritoryID
+INNER JOIN
+	Person.CountryRegion AS pcr ON sst.CountryRegionCode = pcr.CountryRegionCode;
